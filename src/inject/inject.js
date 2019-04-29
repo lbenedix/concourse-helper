43d53bef1772e1997de1bcbf41ebb9e24af08cfe
lbenedix <lbenedix@googlemail.com>
2019-04-29 13:24:52 +0200

    trigger build with ENTER, show dashboard with 'd'

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,6 +6,8 @@ chrome.extension.sendMessage({}, function(response) {
             console.info('Concourse Helper loaded');
             clearInterval(readyStateCheckInterval);
 
+            var region = window.location.host.split('.')[2]
+
             document.onkeyup = function(evt) {
 
                 console.info(evt);
@@ -14,16 +16,18 @@ chrome.extension.sendMessage({}, function(response) {
 
                 if (evt.key === "ArrowRight") {
                     var current_build = document.querySelector('#builds > li.current');
-                    current_build.nextSibling.querySelector('a').click()
+                    current_build.nextSibling.querySelector('a').click();
                 } else if (evt.key === "ArrowLeft") {
                     var current_build = document.querySelector('#builds > li.current');
-                    current_build.previousSibling.querySelector('a').click()
+                    current_build.previousSibling.querySelector('a').click();
                 } else if (evt.key === "ArrowDown") {
                     document.querySelectorAll('.header').forEach(function(it){ it.click();});
                 } else if (evt.key == "ArrowUp") {
                     document.querySelectorAll('.nav-item a')[0].click();
-                } else if (evt.keyu == "Enter" && window.location.href.includes('/jobs/')) {
-                    document.querySelector('button.build-action').click()
+                } else if (evt.key == "Enter" && window.location.href.includes('/jobs/')) {
+                    document.querySelectorAll('button.build-action')[0].click();
+                } else if (evt.key == 'd') {
+                    window.location.pathname = '/dashboard'; 
                 }
             };
         }
