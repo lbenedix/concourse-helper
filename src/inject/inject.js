b0117c68819a2e971bde5af568fce71b653cc347
lbenedix <lbenedix@googlemail.com>
2019-11-28 16:30:16 +0100

    changed stuff for concourse 5

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -6,24 +6,6 @@ chrome.extension.sendMessage({}, function (response) {
             if (document.readyState === "complete") {
                 clearInterval(readyStateCheckInterval);
 
-                if (window.location.pathname == '/dashboard') {
-                    document.getElementById("search-input-field").focus({ preventScroll: true });
-                }
-
-                var region = window.location.host.split('.')[2];
-                if (document.querySelectorAll('#top-bar-app > nav > ul.groups').length > 0) {
-                    var x = document.createElement("li");
-                    x.textContent = region;
-                    x.style.fontSize = "26px";
-                    document.querySelector('#top-bar-app > nav > ul.groups').append(x);
-                } else if (document.querySelectorAll('#elm-app-embed > div > div.module-topbar > div.topbar-logo').length > 0) {
-                    var x = document.createElement("span");
-                    x.textContent = region;
-                    x.style.fontSize = "26px";
-                    x.style.verticalAlign = "top";
-                    document.querySelector('#elm-app-embed > div > div.module-topbar > div.topbar-logo').append(x);
-                }
-
                 document.onkeyup = function (evt) {
                     console.info(evt);
                     evt = evt || window.event;
@@ -35,7 +17,7 @@ chrome.extension.sendMessage({}, function (response) {
                         }
                     }
 
-                    if (window.location.pathname.includes('/builds/')) {
+                    if (window.location.pathname.includes('/builds/') || window.location.pathname.includes('/resources/')) {
                         if (evt.key === "ArrowRight") {
                             var current_build = document.querySelector('#builds > li.current');
                             current_build.nextSibling.querySelector('a').click();
@@ -45,18 +27,12 @@ chrome.extension.sendMessage({}, function (response) {
                         } else if (evt.key === "ArrowDown") {
                             document.querySelectorAll('.header').forEach(function (it) { it.click(); });
                         } else if (evt.key == "Enter") {
-                            document.querySelectorAll('button.build-action')[0].click();
-                        } else if (evt.key == "ArrowUp") {
-                            document.querySelectorAll('.nav-item a')[0].click();
-                        }
-                    }
-
-                    if (!window.location.pathname.includes('/dashboard') &&
-                        !window.location.pathname.includes('/login')) {
-                        if (evt.key == 'd') {
-                            window.location.pathname = '/dashboard';
-                        } else if (evt.key == 'D') {
-                            window.location.pathname = '/dashboard/hd';
+                            if (window.location.pathname.includes('/builds/')) {
+                                document.querySelector('button[title="Trigger Build"]').click();
+                            } else if (window.location.pathname.includes('/resources/')) {
+                                console.log('start resource check');
+                                document.querySelector('div.resource-check-status > div:nth-child(1) > div').click();
+                            }
                         }
                     }
                 };
